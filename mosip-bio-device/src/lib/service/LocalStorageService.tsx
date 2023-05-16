dcecb585711d53035d9fc146660471222541478f
Zeeshan Mehboob <zeeshan.mehboob@infosys.com>
2023-05-16 15:51:39 +0530

    [ADDED] license header in each file

--- mosip-bio-device/src/lib/service/LocalStorageService.tsx
+++ mosip-bio-device/src/lib/service/LocalStorageService.tsx
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import { IDeviceInfo, IDiscoverResponse } from "../models";
 
 const device_info_keyname = "deviceInfo";
