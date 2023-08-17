159c351f367da3cfb5d13a6afaa0f480a547b9f7
Zeeshan Mehboob <zmahboob00@gmail.com>
2023-08-17 16:28:19 +0530

    [REMOVED] storybook-examples

--- storybook-examples/.storybook/manager.ts
+++
@@ -1,9 +0,0 @@
-import { addons } from "@storybook/manager-api";
-import { create } from "@storybook/theming";
-
-addons.setConfig({
-  theme: create({
-    base: "light",
-    brandImage: "https://mosip.io/images/logo.png"
-  }),
-});
