f9884a0107fe6e4e2fa4314536895d1e2cb86bde
Zeeshan Mehboob <zmahboob00@gmail.com>
2023-08-17 12:21:53 +0530

    [INITIAL] storybook version with react & vanilla js

---
+++ storybook-examples/.storybook/preview.ts
@@ -0,0 +1,20 @@
+import { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    options: {
+      storySort: {
+        order: ["JavaScript", "React"]
+      }
+    }
+  },
+};
+
+export default preview;
