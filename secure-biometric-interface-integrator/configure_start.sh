0f15eded721692d624575a14be7e2352c286a4e1
KONIJETI YASWANTHA NAGARJUNA <50859121+YaswanthNagarjuna@users.noreply.github.com>
2023-08-30 19:15:36 +0530

    [ES-122]

--- secure-biometric-interface-integrator/configure_start.sh
+++ secure-biometric-interface-integrator/configure_start.sh
@@ -8,6 +8,14 @@ source .env
 # Download the zip file
 curl -o secure-biometric-interface-integrator-bundle.zip "$artifactory_url_env/artifactory/libs-release-local/i18n/secure-biometric-interface-integrator-bundle.zip"
 
+
+# Check if the downloaded file is a valid zip file
+if ! unzip -t secure-biometric-interface-integrator-bundle.zip &>/dev/null; then
+    echo "[-] Error: The downloaded file is not a valid zip archive."
+    rm secure-biometric-interface-integrator-bundle.zip
+    exit 1
+fi
+
 # Unzip the file
 unzip secure-biometric-interface-integrator-bundle.zip -d extracted_contents
 
