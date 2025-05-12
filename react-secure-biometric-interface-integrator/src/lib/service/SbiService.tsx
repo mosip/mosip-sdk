30f37006b657d10c06a61026efd81efd308a8ba9
sajid.mannikeri <sajid.mannikeri@infosys.com>
2025-05-12 14:58:04 +0530

    Added function to validate number
    Signed-off-by: sajid.mannikeri <sajid.mannikeri@infosys.com>

--- react-secure-biometric-interface-integrator/src/lib/service/SbiService.tsx
+++ react-secure-biometric-interface-integrator/src/lib/service/SbiService.tsx
@@ -114,16 +114,16 @@ class SbiService {
       env: this.esignetConfig.env,
       purpose,
       specVersion,
-      timeout: String(this.esignetConfig.captureTimeout * 1000),
+      timeout: String(getValidNumber(this.esignetConfig.captureTimeout, 30) * 1000), // Default to 30
       captureTime: new Date().toISOString(),
       domainUri: this.esignetConfig.domainUri,
       transactionId,
       bio: [
         {
           type, //modality
-          count: String(count), // from configuration
+          count: String(getValidNumber(count, 1)), // from configuration, default to 1
           bioSubType,
-          requestedScore: String(requestedScore), // from configuration
+          requestedScore: String(getValidNumber(requestedScore, 70)), // from configuration, default to 70
           deviceId, // from discovery
           deviceSubId: "0", //Set as 0, not required for Auth capture.
           previousHash: previousHashValue, // calculated sha256 hash of empty string
@@ -304,6 +304,12 @@ const validateDeviceInfo = (deviceInfo: IDeviceInfo | any): boolean => {
 const decodeJWT = async (signed_jwt: string) => {
   const data = await jose.decodeJwt(signed_jwt);
   return data;
+};
+
+const getValidNumber = (value: any, defaultValue: number): number => {
+  return value === null || value === undefined || isNaN(Number(value))
+    ? defaultValue
+    : Number(value);
 };
 
 export { SbiService };
